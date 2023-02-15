671db7f117ab4621625410a959d93e31fb21a857
TomKovac <61820360+TomKovac@users.noreply.github.com>
2023-02-15 09:44:20 +0100

    19 desing component base class (#29)
    * Sequencer Restore call on step with error state in Step_by_step mode added, when StepIn called
    
    * Arrows added into the currently active step in the sequencer
    
    * Arrows added into the description of the currently active step in the sequencer
    
    * [infrastructure] improvement to cake
    
    * update packages
    
    * sync with dev
    
    * refactoring: Ix prefix added
    
    ---------
    
    Co-authored-by: Peter <61538034+PTKu@users.noreply.github.com>

---
+++ src/core/src/ix.framework.core/IxCoordination/IxSequencer.cs
@@ -0,0 +1,13 @@
+﻿// ix.framework.core
+// Copyright (c)2022 Peter Kurhajec and Contributors All Rights Reserved.
+// Contributors: https://github.com/PTKu/ix/graphs/contributors
+// See the LICENSE file in the repository root for more information.
+// https://github.com/PTKu/ix/blob/master/LICENSE
+// Third party licenses: https://github.com/PTKu/ix/blob/master/notices.md
+
+namespace ix.framework.core;
+
+public partial class IxSequencer
+{
+    
+}
