bc605ba816c1255710178be872d9e7f507e80342
Peter <61538034+PTKu@users.noreply.github.com>
2023-03-14 10:50:51 +0100

    GH_IXF_51 replication

--- src/integrations/tests/integration.tests/_issues/GH_IXF_51.cs
+++ src/integrations/tests/integration.tests/_issues/GH_IXF_51.cs
@@ -7,6 +7,7 @@ using Siemens.Simatic.S7.Webserver.API.Models;
 using Siemens.Simatic.S7.Webserver.API.Services;
 using Siemens.Simatic.S7.Webserver.API.Services.RequestHandling;
 
+// Addressing https://github.com/ix-ax/ix.framework/issues/51
 
 namespace integartions.issues
 {
@@ -16,14 +17,14 @@ namespace integartions.issues
     using Xunit;
     using Xunit.Abstractions;
 
-    public class  BUG_Fails_to_access_inherited_members_in_some_object_composition_scenarios_51
+    public class GH_IXF_51
     {
         private DataExchangeTestsContext testContext = Entry.Plc.Integrations.DataExchangeTestsContext;
 
         private readonly ITestOutputHelper output;
 
 
-        public BUG_Fails_to_access_inherited_members_in_some_object_composition_scenarios_51(ITestOutputHelper output)
+        public GH_IXF_51(ITestOutputHelper output)
         {
             this.output = output;
         }
@@ -32,18 +33,20 @@ namespace integartions.issues
         public async Task Replicate()
         {
             var serviceFactory = new ApiStandardServiceFactory();
-            var reqHandler = await serviceFactory.GetApiHttpClientRequestHandlerAsync("192.168.0.4", "Everybody", "");
+            var reqHandler = await serviceFactory.GetApiHttpClientRequestHandlerAsync(Environment.GetEnvironmentVariable("AXTARGET"), "Everybody", "");
 
-            // Throws
-            var mystery = Entry.Plc.Integrations.DataExchangeTestsContext.Create.Manager.CreateTask;
-            var works = Entry.Plc.process_data_manager.CreateTask;
-
-            Assert.True(mystery.GetType() == works.GetType());
+            var mystery = Entry.Plc.GH_IXF_51.DataExchangeTestsContext.Create.Manager.CreateTask;
+            var works = Entry.Plc.GH_IXF_51.process_data_manager.CreateTask;
 
             output.WriteLine($"\"TGlobalVariablesDB\".{works.Status.Symbol}");
             output.WriteLine($"\"TGlobalVariablesDB\".{mystery.Status.Symbol}");
 
-            var apiBrowseResponse = await reqHandler.PlcProgramBrowseAsync(ApiPlcProgramBrowseMode.Var, $"\"TGlobalVariablesDB\".{works.Status.Symbol}");
+
+            // Both objects are of the same tyep
+            Assert.True(mystery.GetType() == works.GetType());
+            
+            // Works
+            await reqHandler.PlcProgramBrowseAsync(ApiPlcProgramBrowseMode.Var, $"\"TGlobalVariablesDB\".{works.Status.Symbol}");
 
             await Assert.ThrowsAsync<Siemens.Simatic.S7.Webserver.API.Exceptions.ApiInvalidAddressException>(async () => await reqHandler.PlcProgramBrowseAsync(ApiPlcProgramBrowseMode.Var, $"\"TGlobalVariablesDB\".{mystery.Status.Symbol}"));
         }
