6167a86eb694d9e4d54b433a0b0a4c1a20a224df
TomKovac <61820360+TomKovac@users.noreply.github.com>
2023-05-03 13:34:49 +0200

    AxoSequencerContainer

---
+++ src/core/src/AXOpen.Core.Blazor/AxoCoordination/AxoSequencerExtended/AxoSequencerContainerView.razor.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using AXSharp.Connector;
+using Microsoft.AspNetCore.Components;
+
+namespace AXOpen.Core
+{
+    public partial class AxoSequencerContainerView  
+    {
+        public IEnumerable<AxoStep?> Steps => Component.GetKids().OfType<AxoStep>();
+
+        [Parameter] public bool IsControllable { get; set; } = true;
+
+        [Parameter] public bool HasTaskControlButton { get; set; } = true;
+
+        [Parameter] public bool HasSettings { get; set; } = true;
+
+        [Parameter] public bool HasStepControls { get; set; } = true;
+
+        [Parameter] public bool HasStepDetails { get; set; } = true;
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            this.UpdateValuesOnChange(Component);
+        }
+    }
+
+    public class AxoSequencerContainerCommandView : AxoSequencerContainerView
+    {
+        public AxoSequencerContainerCommandView()
+        {
+            IsControllable = true;
+        }
+    }
+
+    public class AxoSequencerContainerStatusView : AxoSequencerContainerView
+    {
+        public AxoSequencerContainerStatusView()
+        {
+            IsControllable = false;
+        }
+    }
+}
