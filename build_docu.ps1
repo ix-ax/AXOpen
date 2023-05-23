db59cb32fad57d56c046b3b61806d83c1a6f5ccf
Peter <61538034+PTKu@users.noreply.github.com>
2023-05-23 07:31:47 +0200

    updates to v0.14.0-alpha.106

--- build_docu.ps1
+++ build_docu.ps1
@@ -1,6 +1,4 @@
 # dotnet run --project ..\ix\src\ix.compiler\src\ixd\Ix.ixd.csproj --framework net7.0 -x .\src\core\ctrl\ -o .\docfx\apictrl\
-dotnet ixd -x .\src\core\ctrl\ -o .\docfx\apictrl\core\
-dotnet ixd -x .\src\messaging\ctrl\ -o .\docfx\apictrl\messaging\
-dotnet ixd -x .\src\abstractions\ctrl\ -o .\docfx\apictrl\abstractions\
+dotnet ixd -x .\src\abstractions\ctrl .\src\data\ctrl\ .\src\core\ctrl .\src\messaging\ctrl  -o .\docfx\apictrl\
 dotnet docfx .\docfx\docfx.json --debug
 dotnet docfx serve .\docs\
