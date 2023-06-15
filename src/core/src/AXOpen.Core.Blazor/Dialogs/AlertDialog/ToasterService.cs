6a208cb83eb6a28671be63765d8e03e9a7d82ec2
Brano5 <97288158+Brano5@users.noreply.github.com>
2023-06-15 13:45:37 +0200

    Alert dialog implementation (#107)
    * WIP - initial implementations of dialogs
    
    * Working on AlertDialogs
    
    * Alert Dialog implementation
    
    * Update to 0.14.0-alpha.109
    
    * Dialogs cleanup
    
    * Remove vmTest
    
    * AlertDialogService in AxoDataViewModelTests
    
    * Updated packages

---
+++ src/core/src/AXOpen.Core.Blazor/Dialogs/AlertDialog/ToasterService.cs
@@ -0,0 +1,79 @@
+﻿using AXSharp.Abstractions.Dialogs.AlertDialog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Timers;
+
+namespace AXSharp.Presentation.Blazor.Controls.Dialogs.AlertDialog
+{
+    public class ToasterService : IAlertDialogService, IDisposable
+    {
+        private List<IAlertDialog> ToastsList { get; set; } = new List<IAlertDialog>();
+        private System.Timers.Timer Timer = new System.Timers.Timer();
+
+        public event EventHandler? AlertDialogChanged;
+
+        public ToasterService()
+        {
+            Timer.Interval = 1000;
+            Timer.AutoReset = true;
+            Timer.Elapsed += TimerElapsed;
+            Timer.Start();
+        }
+
+        public void AddAlertDialog(string type, string title, string message, int time)
+        {
+            ToastsList.Add(new AlertDialog(type, title, message, time));
+            AlertDialogChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void AddAlertDialog(IAlertDialog toast)
+        {
+            ToastsList.Add(toast);
+            AlertDialogChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public List<IAlertDialog> GetAlertDialogs()
+        {
+            ClearBurntAlertDialog();
+            return ToastsList;
+        }
+
+        public void RemoveAlertDialog(IAlertDialog toast)
+        {
+            ToastsList.Remove(toast);
+            AlertDialogChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void RemoveAllAlertDialogs()
+        {
+            ToastsList.Clear();
+            AlertDialogChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void ClearBurntAlertDialog()
+        {
+            var toastsToDelete = ToastsList.Where(item => item.TimeToBurn < DateTimeOffset.Now).ToList();
+            if (toastsToDelete != null && toastsToDelete.Count > 0)
+            {
+                toastsToDelete.ForEach(toast => ToastsList.Remove(toast));
+                AlertDialogChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        private void TimerElapsed(object? sender, ElapsedEventArgs e)
+        {
+            ClearBurntAlertDialog();
+            AlertDialogChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void Dispose()
+        {
+            if (Timer != null)
+            {
+                Timer.Elapsed += TimerElapsed;
+                Timer.Stop();
+            }
+        }
+    }
+}
