4639d0217f44e7fdc9f0253b105077869b531c85
Specter-13 <56168909+Specter-13@users.noreply.github.com>
2023-07-04 15:30:40 +0200

    wip on view

--- src/core/src/AXOpen.Core.Blazor/AxoDialogs/AxoActionDialog/AxoDialogActionView.razor.cs
+++ src/core/src/AXOpen.Core.Blazor/AxoDialogs/AxoActionDialog/AxoDialogActionView.razor.cs
@@ -15,31 +15,43 @@ namespace AXOpen.Core
 
         
         public IsDialog Dialog { get; private set; }
-       
+
+
+        private bool IsOkDialogType() => Component._hasOK.Cyclic;
+        
+
+        private bool IsYesNoDialogType() => Component._hasYes.Cyclic && Component._hasNo.Cyclic;
+        
+
+        private bool IsYesNoCancelDialogType() => Component._hasYes.Cyclic && Component._hasNo.Cyclic && Component._hasCancel.Cyclic;
+        
 
         public async Task DialogAnswerOk()  
         {
             Component._answer.Edit = (short)eDialogAnswer.OK;
             //await HideOffcanvasAsync();
             await CloseDialog();
-       
+            //AxoApplication.Current.Logger.Information($"{nameof(DialogAnswerOk)} of {Component.HumanReadable} was executed @{{payload}}.", new { Component.Symbol });
             //Dialog = null;
             //await InvokeAsync(StateHasChanged);
             //TcoAppDomain.Current.Logger.Information($"{nameof(DialogAnswerOk)} of {Dialog.HumanReadable} was executed @{{payload}}.", new { Dialog.Symbol });
         }
-        public void DialogAnswerYes()
+        public async Task DialogAnswerYes()
         {
             Component._answer.Edit = (short)eDialogAnswer.Yes;
+            await CloseDialog();
             //TcoAppDomain.Current.Logger.Information($"{nameof(DialogAnswerYes)} of {Dialog.HumanReadable} was executed @{{payload}}.", new { Dialog.Symbol });
         }
-        public void DialogAnswerNo()
+        public async Task DialogAnswerNo()
         {
             Component._answer.Edit = (short)eDialogAnswer.No;
+            await CloseDialog();
             //TcoAppDomain.Current.Logger.Information($"{nameof(DialogAnswerNo)} of {Dialog.HumanReadable} was executed @{{payload}}.", new { Dialog.Symbol });
         }
-        public void DialogAnswerCancel()
+        public async Task DialogAnswerCancel()
         {
             Component._answer.Edit = (short)eDialogAnswer.Cancel;
+            await CloseDialog();
             //TcoAppDomain.Current.Logger.Information($"{nameof(DialogAnswerCancel)} of {Dialog.HumanReadable} was executed @{{payload}}.", new { Dialog.Symbol });
         }
     }
