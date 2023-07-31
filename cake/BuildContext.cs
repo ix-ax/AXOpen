09fdc833500f35c836fef6b08533e4e378dbca5c
Specter-13 <56168909+Specter-13@users.noreply.github.com>
2023-07-31 15:17:31 +0200

    added first implementation of analogue. digital inspectors, added comprehensive result, wip on data inspectors

--- cake/BuildContext.cs
+++ cake/BuildContext.cs
@@ -144,6 +144,7 @@ public class BuildContext : FrostingContext
         ("core", "axopen.core"),       
         ("data", "axopen.data"),
         ("probers", "axopen.probers"),
+        ("inspectors", "axopen.inspectors"),
     };
 
     public IEnumerable<(string folder, string name, string targetIp, string targetPlatform)> Integrations { get; } = new[]
