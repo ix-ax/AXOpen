217470255c9a5bd2aeb5d8f2e2c64eda96eca336
TomKovac <tomas.kovac@mts.sk>
2023-08-07 14:11:25 +0200

    Excessive ResultData size checked

--- src/components.cognex.vision/src/AXOpen.Cognex.Vision/AxoDataman/v_6_0_0/AxoDataman.cs
+++ src/components.cognex.vision/src/AXOpen.Cognex.Vision/AxoDataman/v_6_0_0/AxoDataman.cs
@@ -42,6 +42,7 @@ namespace AXOpen.Cognex.Vision.v_6_0_0_0
                     errorDescriptionDict.Add(709    ,"Error: ResultData-lower bound index is not zero!");
                     errorDescriptionDict.Add(710    ,"Error: UserData has invalid size!");
                     errorDescriptionDict.Add(711    ,"Error: UserData-lower bound index is not zero!");
+                    errorDescriptionDict.Add(712    ,"Error: The ResultData length exceeds the configured hardware structure's length!");
 
                 }
                 string errorDescription = "   ";
@@ -64,15 +65,17 @@ namespace AXOpen.Cognex.Vision.v_6_0_0_0
                 if (actionDescriptionDict == null) { actionDescriptionDict = new Dictionary<uint, string>(); }
                 if (actionDescriptionDict.Count == 0)
                 {
-                    actionDescriptionDict.Add(0     ,"   ");
-                    actionDescriptionDict.Add(300   ,"Restore executed.");
-                    actionDescriptionDict.Add(301   ,"Clearing of the result data started.");
-                    actionDescriptionDict.Add(302   ,"Clearing of the result data running.");
-                    actionDescriptionDict.Add(303   ,"Clearing of the result data was completed successfully.");
-                    actionDescriptionDict.Add(304   ,"Reading started.");
-                    actionDescriptionDict.Add(305   ,"Reading running.");
-                    actionDescriptionDict.Add(306   ,"Reading was completed successfully.");
-                    actionDescriptionDict.Add(307   ,"Continous reading active: New data read.");
+                    actionDescriptionDict.Add(0, "   ");
+                    actionDescriptionDict.Add(300, "Restore executed.");
+                    actionDescriptionDict.Add(301, "Clearing of the result data started.");
+                    actionDescriptionDict.Add(302, "Clearing of the result data running.");
+                    actionDescriptionDict.Add(303, "Clearing of the result data was completed successfully.");
+                    actionDescriptionDict.Add(304, "Reading started.");
+                    actionDescriptionDict.Add(305, "Reading running.");
+                    actionDescriptionDict.Add(306, "Reading was completed successfully.");
+                    actionDescriptionDict.Add(307, "Continous reading active: New data read.");
+                    actionDescriptionDict.Add(308, "Clearing of the result data restored.");
+                    actionDescriptionDict.Add(309, "Reading restored.");
 
 
                     actionDescriptionDict.Add(600   ,"Clearing of the result data was aborted, while not yet completed!");
