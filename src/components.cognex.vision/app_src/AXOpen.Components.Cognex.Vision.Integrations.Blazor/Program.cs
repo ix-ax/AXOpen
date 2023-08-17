eba8a6b726d9f2204c1074ad352b7091cc32e53d
Tomas Kovac <tomas.kovac@mts.sk>
2023-08-17 08:43:50 +0200

    wip

--- src/components.cognex.vision/app_src/AXOpen.Components.Cognex.Vision.Integrations.Blazor/Program.cs
+++ src/components.cognex.vision/app_src/AXOpen.Components.Cognex.Vision.Integrations.Blazor/Program.cs
@@ -1,5 +1,4 @@
 using AXOpen;
-//using axopen_integrations_blazor.Data;
 using axopen_components_cognex_vision_integrations;
 using AXSharp.Connector;
 using AXOpen.Data.Json;
@@ -21,13 +20,9 @@ namespace axopen_components_cognex_vision_integrations_blazor
             // Add services to the container.
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
-            //builder.Services.AddSingleton<WeatherForecastService>();
             builder.Services.AddAxoCoreServices();
             builder.Services.AddIxBlazorServices();
 
-            //builder.Services.AddScoped<IAlertDialogService, ToasterService>();
-
-            //builder.Services.AddTcoCoreExtensions();
 
             AxoApplication.CreateBuilder().ConfigureLogger(new SerilogLogger(new LoggerConfiguration()
                 .WriteTo.Console().MinimumLevel.Debug()
@@ -53,76 +48,9 @@ namespace axopen_components_cognex_vision_integrations_blazor
             Entry.Plc.Connector.SubscriptionMode = AXSharp.Connector.ReadSubscriptionMode.Polling;
 
             await Entry.Plc.Connector.IdentityProvider.ConstructIdentitiesAsync();
-
-            //var repository = Ix.Repository.Json.Repository.Factory(new AXOpen.Data.Json.JsonRepositorySettings<Pocos.AxoDataExamples.AxoProductionData>(Path.Combine(Environment.CurrentDirectory, "data", "processdata")));
-            //var repository2 = Ix.Repository.Json.Repository.Factory(new AXOpen.Data.Json.JsonRepositorySettings<Pocos.AxoDataExamples.AxoTestData>(Path.Combine(Environment.CurrentDirectory, "data", "testdata")));
-            ////inherited IxProductionData
-            ////var repository = Ix.Repository.Json.Repository.Factory(new AXOpen.Data.Json.JsonRepositorySettings<Pocos.AxoDataExamples.AxoProductionDataInherited>(Path.Combine(Environment.CurrentDirectory, "data", "processdata")));
-
-            //// Entry.Plc.process_data_manager.InitializeRepository(repository);
-
-            //Entry.Plc.MainContext.process_data_manager.InitializeRemoteDataExchange(repository);
-            //Entry.Plc.MainContext.test_data_manager.InitializeRemoteDataExchange(repository2);
-
-            
-            //Entry.Plc.Integrations.DM
-            //    .InitializeRemoteDataExchange(
-            //        Ix.Repository.Json.
-            //            Repository.Factory
-            //                (new AXOpen.Data.Json.JsonRepositorySettings<Pocos.IntegrationLightDirect.DataSet>(Path.Combine(Environment.CurrentDirectory, "data", "processdata1"))));
-
-            //var pdfBuilder =
-            //    Entry.Plc.Integrations.DataFragmentContext.PD.CreateBuilder<IntegrationAxoDataFramentsExchange.ProcessData>();
-
-            //pdfBuilder.Set.SetRepository(new JsonRepository<Pocos.IntegrationAxoDataFramentsExchange.SharedProductionData>(
-            //    new AXOpen.Data.Json.JsonRepositorySettings<Pocos.IntegrationAxoDataFramentsExchange.SharedProductionData>(Path.Combine(Environment.CurrentDirectory, "bin", "data-framents", "set"))));
-            //pdfBuilder.Manip.SetRepository(
-            //    new JsonRepository<Pocos.IntegrationAxoDataFramentsExchange.FragmentProcessData>( new AXOpen.Data.Json.JsonRepositorySettings<Pocos.IntegrationAxoDataFramentsExchange.FragmentProcessData>(Path.Combine(Environment.CurrentDirectory, "bin", "data-framents", "fm"))));
-
-
-            ////<AxoDataExampleDocuIntialization>
-            //var exampleRepositorySettings =
-            //    new AXOpen.Data.Json.JsonRepositorySettings<Pocos.AxoDataExamplesDocu.AxoProductionData>(
-            //        Path.Combine(Environment.CurrentDirectory, "exampledata"));
-
-            //var exampleRepository = 
-            //    Ix.Repository.Json.Repository.Factory(exampleRepositorySettings);
-            
-            //Entry.Plc.AxoDataExamplesDocu.DataManager.InitializeRemoteDataExchange(exampleRepository);
-            ////</AxoDataExampleDocuIntialization>
-
-
-            ////<AxoDataFragmentedExampleDocuIntialization>
-
-            //var scatteredDataBuilder =
-            //    Entry.Plc.AxoDataFragmentExchangeContext.ProcessData.CreateBuilder<AxoDataFramentsExchangeDocuExample.ProcessDataManager>();
-
-            //// Setting up repositories
-            //scatteredDataBuilder.SharedHeader.SetRepository(new JsonRepository<Pocos.AxoDataFramentsExchangeDocuExample.SharedDataHeaderData>(
-            //    new AXOpen.Data.Json.JsonRepositorySettings<Pocos.AxoDataFramentsExchangeDocuExample.SharedDataHeaderData>(Path.Combine(Environment.CurrentDirectory, "bin", "data-framents-docu", "set"))));
-            //scatteredDataBuilder.Station_1.SetRepository(
-            //    new JsonRepository<Pocos.AxoDataFramentsExchangeDocuExample.Station_1_Data>(
-            //        new AXOpen.Data.Json.JsonRepositorySettings<Pocos.AxoDataFramentsExchangeDocuExample.Station_1_Data>(Path.Combine(Environment.CurrentDirectory, "bin", "data-framents", "fm"))));
-            ////</AxoDataFragmentedExampleDocuIntialization>
-
-
             //<AxoAppBuilder>
             var axoAppBuilder = AxoApplication.CreateBuilder();
             //</AxoAppBuilder>
-
-            //<AxoLoggerConfiguration>
-
-            // Creates serilog logger with single sink to Console window.
-            
-            //axoAppBuilder.ConfigureLogger(new SerilogLogger(new LoggerConfiguration()
-            //    .WriteTo.Console().MinimumLevel.Verbose()
-            //    .CreateLogger()));
-            ////</AxoLoggerConfiguration>
-
-            ////<AxoLoggerInitialization>
-            //Entry.Plc.AxoLoggers.LoggerOne.StartDequeuing(AxoApplication.Current.Logger, 250);
-            ////</AxoLoggerInitialization>
-
 
 
             var app = builder.Build();
@@ -143,10 +71,6 @@ namespace axopen_components_cognex_vision_integrations_blazor
 
             app.MapBlazorHub();
             app.MapFallbackToPage("/_Host");
-
-            //#region InitializeRemoteTask
-            //Entry.Plc.AxoRemoteTasks._remoteTask.Initialize(() => Console.WriteLine($"Remote task executed PLC sent this string: '{Entry.Plc.AxoRemoteTasks._remoteTask.Message.GetAsync().Result}'"));
-            //#endregion
 
             app.Run();
 
--- src/components.cognex.vision/src/AXOpen.Cognex.Vision.Blazor/AxoByteArray255/AxoByteArray255View.razor.cs
+++ src/components.cognex.vision/src/AXOpen.Cognex.Vision.Blazor/AxoByteArray255/AxoByteArray255View.razor.cs
@@ -1,21 +1,97 @@
-﻿using System.IO;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
 using System.Net.Http.Headers;
 using System.Reflection;
 using System.Security.Principal;
 using AXSharp.Connector;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
-
+using static System.Formats.Asn1.AsnWriter;
+using System;
+using AXSharp.Connector;
+using AXSharp.Connector.ValueTypes;
+using System.Collections.Generic;
+using System.Text;
 
 namespace AXOpen.Cognex.Vision.v_6_0_0_0
 {
     public partial class AxoByteArray255View
     {
+        public enum eDisplayFormat { Array_of_decimals, Array_of_hexdecimals, String };
 
+        private eDisplayFormat _currentDisplayFormat;
+        public eDisplayFormat CurrentDisplayFormat
+        {
+            get => _currentDisplayFormat;
+            set
+            {
+                _currentDisplayFormat = value;
+                UpdateAndFormatData();
+            }
+        }
+        public ObservableCollection<IndexedData<string>> Data { get; private set; }
         protected override void OnInitialized()
         {
             UpdateValuesOnChange(Component);
             base.OnInitialized();
+            UpdateAndFormatData();
+            Component.DataChanged.Subscribe((sender, arg) => UpdateAndFormatData());
+        }
+
+        private async void UpdateAndFormatData()
+        {
+                try
+                {
+                    if (Data == null)
+                    {
+                        Data = new ObservableCollection<IndexedData<string>>();
+                    }
+                    else
+                    {
+                        Data.Clear();
+                    }
+                    if (Component != null && Component.GetConnector() != null && Component.Data != null)
+                    {
+
+                        int length = Component.Data.Length;
+                        length = 255;
+                        if (CurrentDisplayFormat == eDisplayFormat.Array_of_decimals)
+                        {
+                            for (int i = 0; i < length; i++)
+                            {
+                                byte _item = await Component.Data[i].GetAsync();
+                                Data.Add(new IndexedData<string>(i, _item.ToString()));
+                            }
+                        }
+                        else if (CurrentDisplayFormat == eDisplayFormat.Array_of_hexdecimals)
+                        {
+                            for (int i = 0; i < length; i++)
+                            {
+                                byte _item = await Component.Data[i].GetAsync();
+                                Data.Add(new IndexedData<string>(i, _item.ToString("X")));
+                            }
+                        }
+                        else if (CurrentDisplayFormat == eDisplayFormat.String)
+                        {
+                            for (int i = 0; i < length; i++)
+                            {
+                                byte _byte = await Component.Data[i].GetAsync();
+                                string _string = "";
+                                if (_byte > 0)
+                                    _string = Encoding.UTF8.GetString(new byte[] { _byte });
+                                else _string = "N/A";
+
+                                Data.Add(new IndexedData<string>(i, _string));
+                            }
+                        }
+                    }
+                }
+                catch (System.Exception ex)
+                {
+
+                    throw;
+                }
         }
     }
 
