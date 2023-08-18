3d791c52545fadf82f5a963048c3f05b22849ed3
Tomas Kovac <tomas.kovac@mts.sk>
2023-08-18 14:54:06 +0200

    Result data async read on demand

--- src/components.cognex.vision/src/AXOpen.Cognex.Vision.Blazor/AxoByteArray255/AxoByteArray255View.razor.cs
+++ src/components.cognex.vision/src/AXOpen.Cognex.Vision.Blazor/AxoByteArray255/AxoByteArray255View.razor.cs
@@ -1,98 +1,87 @@
-﻿using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.IO;
-using System.Net.Http.Headers;
-using System.Reflection;
-using System.Security.Principal;
-using AXSharp.Connector;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
-using static System.Formats.Asn1.AsnWriter;
-using System;
-using AXSharp.Connector;
+﻿using AXSharp.Connector;
 using AXSharp.Connector.ValueTypes;
-using System.Collections.Generic;
 using System.Text;
 
 namespace AXOpen.Cognex.Vision.v_6_0_0_0
 {
-    public partial class AxoByteArray255View
+    public partial class AxoByteArray255View : IDisposable
     {
         public enum eDisplayFormat { Array_of_decimals, Array_of_hexdecimals, String };
 
         private eDisplayFormat _currentDisplayFormat;
+
         public eDisplayFormat CurrentDisplayFormat
         {
             get => _currentDisplayFormat;
             set
             {
                 _currentDisplayFormat = value;
-                UpdateAndFormatData();
+                UpdateAndFormatData(null, null);
             }
         }
-        public ObservableCollection<IndexedData<string>> Data { get; private set; }
+        public IndexedData<string>[] Data { get; private set; } = new IndexedData<string>[255];
+        public override void AddToPolling(ITwinElement element, int pollingInterval = 250)
+        {
+            
+        }
+
         protected override void OnInitialized()
         {
             UpdateValuesOnChange(Component);
             base.OnInitialized();
-            //UpdateAndFormatData();
-            Component.DataChanged.Subscribe((sender, arg) => UpdateAndFormatData());
+            UpdateAndFormatData(null, null);
+            Component.DataChanged.Subscribe(UpdateAndFormatData);
         }
 
-        private async void UpdateAndFormatData()
+        private async void UpdateAndFormatData(ITwinPrimitive sender, ValueChangedEventArgs args)
         {
-                try
+            try
+            {
+                if (Component != null && Component.GetConnector() != null && Component.Data != null)
                 {
-                    if (Data == null)
+                    await Component.ReadAsync();
+                    int length = 255;
+                    if (CurrentDisplayFormat == eDisplayFormat.Array_of_decimals)
                     {
-                        Data = new ObservableCollection<IndexedData<string>>();
-                    }
-                    else
-                    {
-                        Data.Clear();
-                    }
-                    if (Component != null && Component.GetConnector() != null && Component.Data != null)
-                    {
-
-                        await Component.ReadAsync();
-                        int length = Component.Data.Length;
-                        length = 255;
-                        if (CurrentDisplayFormat == eDisplayFormat.Array_of_decimals)
+                        for (int i = 0; i < length; i++)
                         {
-                            for (int i = 0; i < length; i++)
-                            {
-                                byte _item = Component.Data[i].LastValue;
-                                Data.Add(new IndexedData<string>(i, _item.ToString()));
-                            }
+                            byte _item = Component.Data[i].LastValue;
+                            Data[i] = new IndexedData<string>(i, _item.ToString());
                         }
-                        else if (CurrentDisplayFormat == eDisplayFormat.Array_of_hexdecimals)
+                    }
+                    else if (CurrentDisplayFormat == eDisplayFormat.Array_of_hexdecimals)
+                    {
+                        for (int i = 0; i < length; i++)
                         {
-                            for (int i = 0; i < length; i++)
-                            {
-                                byte _item = Component.Data[i].LastValue;
-                                Data.Add(new IndexedData<string>(i, _item.ToString("X")));
-                            }
+                            byte _item = Component.Data[i].LastValue;
+                            Data[i] = new IndexedData<string>(i, _item.ToString("X"));
                         }
-                        else if (CurrentDisplayFormat == eDisplayFormat.String)
+                    }
+                    else if (CurrentDisplayFormat == eDisplayFormat.String)
+                    {
+                        for (int i = 0; i < length; i++)
                         {
-                            for (int i = 0; i < length; i++)
-                            {
-                                byte _byte = Component.Data[i].LastValue;
-                                string _string = "";
-                                if (_byte > 0)
-                                    _string = Encoding.UTF8.GetString(new byte[] { _byte });
-                                else _string = "N/A";
-
-                                Data.Add(new IndexedData<string>(i, _string));
-                            }
+                            byte _byte = Component.Data[i].LastValue;
+                            string _string = "";
+                            if (_byte > 0)
+                                _string = Encoding.UTF8.GetString(new byte[] { _byte });
+                            else _string = "N/A";
+                            Data[i] = new IndexedData<string>(i, _string);
                         }
                     }
                 }
-                catch (System.Exception ex)
-                {
+            }
+            catch (System.Exception ex)
+            {
 
-                    throw;
-                }
+                throw;
+            }
+        }
+
+        public override void Dispose()
+        {
+            Component.DataChanged.UnSubscribe(UpdateAndFormatData);
+            base.Dispose();
         }
     }
 
