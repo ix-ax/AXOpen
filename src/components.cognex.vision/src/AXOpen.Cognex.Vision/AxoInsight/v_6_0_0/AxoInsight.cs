bd4a3cdea04d73447e095fba4d8e8f1f60f1426a
Tomas Kovac <tomas.kovac@mts.sk>
2023-08-29 17:24:26 +0200

    Cognex insight: added messengers

--- src/components.cognex.vision/src/AXOpen.Cognex.Vision/AxoInsight/v_6_0_0/AxoInsight.cs
+++ src/components.cognex.vision/src/AXOpen.Cognex.Vision/AxoInsight/v_6_0_0/AxoInsight.cs
@@ -1,9 +1,98 @@
 ﻿using AXOpen.Components.Abstractions;
+using AXOpen.Messaging.Static;
+using AXSharp.Connector;
+using System;
+using System.Collections.Generic;
 
 namespace AXOpen.Cognex.Vision.v_6_0_0_0
 {
     public partial class AxoInsight
     {
+        partial void PostConstruct(ITwinObject parent, string readableTail, string symbolTail)
+        {
+            try
+            {
+                InitializeMessenger();
+                InitializeTaskMessenger();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        private void InitializeMessenger()
+        {
+            List<KeyValuePair<ulong, AxoMessengerTextItem>> messengerTextList = new List<KeyValuePair<ulong, AxoMessengerTextItem>>
+            {
+                new KeyValuePair<ulong, AxoMessengerTextItem>(0, new AxoMessengerTextItem("  ", "  ")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(700, new AxoMessengerTextItem("Parent has NULL reference!",                   "Check the call of the `Run` method, if the `parent` parameter is assigned.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(701, new AxoMessengerTextItem("refAcquisitionControl has NULL reference!",    "Check the call of the `Run` method, if the `refAcquisitionControl` parameter is assigned.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(702, new AxoMessengerTextItem("refAcquisitionStatus has NULL reference!",     "Check the call of the `Run` method, if the `refAcquisitionStatus` parameter is assigned.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(703, new AxoMessengerTextItem("refInspectionControl has NULL reference!",     "Check the call of the `Run` method, if the `refInspectionControl` parameter is assigned.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(704, new AxoMessengerTextItem("refInspectionStatus has NULL reference!",      "Check the call of the `Run` method, if the `refInspectionStatus` parameter is assigned.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(705, new AxoMessengerTextItem("refCommandControl has NULL reference!",        "Check the call of the `Run` method, if the `refCommandControl` parameter is assigned.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(706, new AxoMessengerTextItem("refCommandStatus has NULL reference!",         "Check the call of the `Run` method, if the `refCommandStatus` parameter is assigned.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(707, new AxoMessengerTextItem("refSoftEventControl has NULL reference!",      "Check the call of the `Run` method, if the `refSoftEventControl` parameter is assigned.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(708, new AxoMessengerTextItem("refSoftEventStatus has NULL reference!",       "Check the call of the `Run` method, if the `refSoftEventStatus` parameter is assigned.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(709, new AxoMessengerTextItem("refResultData has NULL reference!",            "Check the call of the `Run` method, if the `refResultData` parameter is assigned.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(710, new AxoMessengerTextItem("refUserData has NULL reference!",              "Check the call of the `Run` method, if the `refUserData` parameter is assigned.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(711, new AxoMessengerTextItem("refResultData has invalid size!",              "Check the size of the `refResultData`.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(712, new AxoMessengerTextItem("refResultData-lower bound index is not zero!", "Check if the `refResultData` parameter is zero-based.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(713, new AxoMessengerTextItem("refUserData has invalid size!",                "Check the size of the `refUserData`.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(714, new AxoMessengerTextItem("refUserData-lower bound index is not zero!",   "Check if the `refUsertData` parameter is zero-based.")),
+
+        };
+
+            _Messenger.DotNetMessengerTextList = messengerTextList;
+        }
+
+        private void InitializeTaskMessenger()
+        {
+            List<KeyValuePair<ulong, AxoMessengerTextItem>> messengerTextList = new List<KeyValuePair<ulong, AxoMessengerTextItem>>
+            {
+                new KeyValuePair<ulong, AxoMessengerTextItem>(0, new AxoMessengerTextItem("  ", "  ")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(600, new AxoMessengerTextItem("Waiting for the signal ExposureComplete to be reseted!"                  ,"Check the status of the `ExposureComplete` signal.")), 
+                new KeyValuePair<ulong, AxoMessengerTextItem>(601, new AxoMessengerTextItem("Waiting for the signal ResultsValid to be reseted!"                      ,"Check the status of the `ResultsValid` signal.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(602, new AxoMessengerTextItem("Waiting for the signal Error to be reseted!"                             ,"Check the status of the `Error` signal.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(603, new AxoMessengerTextItem("Waiting for the signal TriggerReady to be set!"                          ,"Check the status of the `TriggerReady` signal.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(604, new AxoMessengerTextItem("Waiting for the signal TriggerAcknowledge to be set!"                    ,"Check the status of the `TriggerAcknowledge` signal.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(605, new AxoMessengerTextItem("Waiting for the signal InspectionCompleted to be toggled!"               ,"Check the status of the `InspectionCompleted` signal.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(606, new AxoMessengerTextItem("Waiting for the signal ResultsValid to be set!"                          ,"Check the status of the `ResultsValid` signal.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(607, new AxoMessengerTextItem("Waiting for the InspectionResults to be copied!"                         ,"Check the status of the `InspectionResults` data.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(608, new AxoMessengerTextItem("Waiting for the signal CommandExecuting to be reseted!"                  ,"Check the status of the `CommandExecuting` signal.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(609, new AxoMessengerTextItem("Waiting for the signal Online to be reseted!"                            ,"Check the status of the `Online` signal.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(610, new AxoMessengerTextItem("Waiting for the signal Error to be reseted!"                             ,"Check the status of the `Error` signal.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(611, new AxoMessengerTextItem("Waiting for the Job name to be written to User data!"                    ,"Check the status of the `User` data.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(612, new AxoMessengerTextItem("Waiting for the signal ExtendedUserDataSetAcknowledge to be set!"        ,"Check the status of the `ExtendedUserDataSetAcknowledge` signal.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(613, new AxoMessengerTextItem("Waiting for the signal ExtendedUserDataSetAcknowledge to be reseted!"    ,"Check the status of the `ExtendedUserDataSetAcknowledge` signal.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(614, new AxoMessengerTextItem("Waiting for the signal CommandComplete to be set!"                       ,"Check the status of the `CommandComplete` signal.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(615, new AxoMessengerTextItem("Waiting for the signal CommandComplete to be reseted!"                   ,"Check the status of the `CommandComplete` signal.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(616, new AxoMessengerTextItem("Waiting for the signal Online to be set!"                                ,"Check the status of the `Online` signal.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(617, new AxoMessengerTextItem("Waiting for the signal TriggerSoftEvent to be set!"                      ,"Check the status of the `TriggerSoftEvent` signal.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(618, new AxoMessengerTextItem("Waiting for the signal TriggerSoftEvent to be reseted!"                  ,"Check the status of the `TriggerSoftEvent` signal.")),
+                //new KeyValuePair<ulong, AxoMessengerTextItem>(619, new AxoMessengerTextItem("","")),
+                //new KeyValuePair<ulong, AxoMessengerTextItem>(620, new AxoMessengerTextItem("","")),
+                //new KeyValuePair<ulong, AxoMessengerTextItem>(621, new AxoMessengerTextItem("","")),
+                //new KeyValuePair<ulong, AxoMessengerTextItem>(622, new AxoMessengerTextItem("","")),
+                //new KeyValuePair<ulong, AxoMessengerTextItem>(623, new AxoMessengerTextItem("","")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(715, new AxoMessengerTextItem("Empty job name inserted!","Check the required job name.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(716, new AxoMessengerTextItem("Index of UserData in the method SetUserDataAsString exceeds the size hardware structure mapped!","Check the size and the offset of the data written to the User data.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(717, new AxoMessengerTextItem("Index of UserData in the method SetUserDataAsString exceeds the defined size!","Check the size and the offset of the data written to the User data.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(718, new AxoMessengerTextItem("Change job by name failed. Check the value of the InspectionStatus.Error code!","Check the sensor manufacturer documentation.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(719, new AxoMessengerTextItem("Required job number is greater than the maximal value!","Check the sensor manufacturer documentation.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(720, new AxoMessengerTextItem("Change job by number failed. Check the value of the InspectionStatus.Error code!","Check the sensor manufacturer documentation.")),
+                new KeyValuePair<ulong, AxoMessengerTextItem>(721, new AxoMessengerTextItem("Required soft event number is greater than the maximal value of 7!","Check the sensor manufacturer documentation.")),
+                //new KeyValuePair<ulong, AxoMessengerTextItem>(722, new AxoMessengerTextItem("","")),
+                //new KeyValuePair<ulong, AxoMessengerTextItem>(723, new AxoMessengerTextItem("","")),
+                //new KeyValuePair<ulong, AxoMessengerTextItem>(724, new AxoMessengerTextItem("","")),
+                //new KeyValuePair<ulong, AxoMessengerTextItem>(725, new AxoMessengerTextItem("","")),
+                //new KeyValuePair<ulong, AxoMessengerTextItem>(726, new AxoMessengerTextItem("","")),
+
+        };
+
+            _TaskMessenger.DotNetMessengerTextList = messengerTextList;
+        }
     }
 
     public partial class AxoInsight_Status : AxoComponent_Status
@@ -74,16 +163,6 @@ namespace AXOpen.Cognex.Vision.v_6_0_0_0
                     //errorDescriptionDict.Add(7, "");
                     //errorDescriptionDict.Add(7, "");
                     //errorDescriptionDict.Add(7, "");
-                    //errorDescriptionDict.Add(7, "");
-                    //errorDescriptionDict.Add(7, "");
-                    //errorDescriptionDict.Add(7, "");
-                    //errorDescriptionDict.Add(7, "");
-                    //errorDescriptionDict.Add(7, "");
-                    //errorDescriptionDict.Add(7, "");
-                    //errorDescriptionDict.Add(7, "");
-                    //errorDescriptionDict.Add(7, "");
-
-
 
                 }
                 string errorDescription = "   ";
@@ -136,25 +215,13 @@ namespace AXOpen.Cognex.Vision.v_6_0_0_0
                     actionDescriptionDict.Add(602, "Change job by name was aborted, while not yet completed!");
                     actionDescriptionDict.Add(603, "Change job by number was aborted, while not yet completed!");
                     actionDescriptionDict.Add(604, "Soft event was aborted, while not yet completed!");
-                    //actionDescriptionDict.Add(605, "");
-                    //actionDescriptionDict.Add(606, "");
-                    //actionDescriptionDict.Add(605, "");
-                    //actionDescriptionDict.Add(605, "");
-                    //actionDescriptionDict.Add(605, "");
-                    //actionDescriptionDict.Add(605, "");
-                    //actionDescriptionDict.Add(605, "");
-                    //actionDescriptionDict.Add(605, "");
-                    //actionDescriptionDict.Add(605, "");
-                    //actionDescriptionDict.Add(605, "");
-                    //actionDescriptionDict.Add(605, "");
-                    //actionDescriptionDict.Add(605, "");
-                    //actionDescriptionDict.Add(605, "");
-                    //actionDescriptionDict.Add(605, "");
-                    //actionDescriptionDict.Add(605, "");
-                    //actionDescriptionDict.Add(605, "");
-                    //actionDescriptionDict.Add(605, "");
-                    //actionDescriptionDict.Add(605, "");
-                    //actionDescriptionDict.Add(605, "");
+                    //actionDescriptionDict.Add(6, "");
+                    //actionDescriptionDict.Add(6, "");
+                    //actionDescriptionDict.Add(6, "");
+                    //actionDescriptionDict.Add(6, "");
+                    //actionDescriptionDict.Add(6, "");
+                    //actionDescriptionDict.Add(6, "");
+
 
                     actionDescriptionDict.Add(700, "Clearing of the inspection results finished with error!");
                     actionDescriptionDict.Add(701, "Reading finished with error!");
@@ -182,3 +249,4 @@ namespace AXOpen.Cognex.Vision.v_6_0_0_0
         }
     }
 }
+
