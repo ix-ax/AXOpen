8c2b4a25091d6e4863575ccc3bd0fde2e4dfd28a
Branko Zachemsky <bzachemsky@gmail.com>
2023-10-05 15:51:21 +0200

    Template simple improvement

--- src/core/src/AXOpen.Core.Blazor/AxoCoordination/AxoStep/AxoStepView.razor.cs
+++ src/core/src/AXOpen.Core.Blazor/AxoCoordination/AxoStep/AxoStepView.razor.cs
@@ -8,52 +8,11 @@ namespace AXOpen.Core
 {
     public partial class AxoStepView : RenderableComplexComponentBase<AxoStep>
     {
-        private string StepRowColor => UpdateStepRowColors();
-
         private bool IsActive => Component.IsActive.Cyclic == true;
-
-        private string Description
-        {
-            get
-            {
-                var text = string.IsNullOrEmpty(Component.StepDescription.Cyclic)
-                    ? Component.Order.Cyclic.ToString()
-                    : Component.StepDescription.Cyclic;
-
-                if (IsActive)
-                {
-                    return $">> {text} <<";
-                }
-
-                return text;
-            }
-        }
-
-        private string UpdateStepRowColors()
-        {
-            switch ((eAxoTaskState)Component.Status.Cyclic)
-            {
-                case eAxoTaskState.Disabled:
-                    return "bg-secondary text-white";
-                case eAxoTaskState.Ready:
-                    return"bg-primary text-white";
-                case eAxoTaskState.Kicking:
-                    return "bg-light text-dark";
-                case eAxoTaskState.Busy:
-                    return "bg-warning text-dark";
-                case eAxoTaskState.Done:
-                    return "bg-success text-white";
-                case eAxoTaskState.Error:
-                    return "bg-danger text-white";
-                default:
-                    return "bg-white text-dark";
-            }
-        }
 
         protected override void OnInitialized()
         {
             UpdateValuesOnChange(Component,350);
-            UpdateStepRowColors();
         }
 
         [Parameter]
