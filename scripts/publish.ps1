2099967a459c1c11c37677a0d71598917e20ca80
Peter <61538034+PTKu@users.noreply.github.com>
2023-10-07 11:08:24 +0200

    version bump to 0.4.1

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -1,4 +1,4 @@
 # run build
 
-dotnet run --project cake/Build.csproj --do-test --do-pack --do-publish --test-level 100
+dotnet run --project cake/Build.csproj --do-test --do-pack --do-publish --test-level 1 -n -x
 exit $LASTEXITCODE;
