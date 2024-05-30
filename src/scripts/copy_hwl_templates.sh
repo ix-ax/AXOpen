a5fdd04b3e525fa31220de2c24c01bbb3cd7243c
PTKu <61538034+PTKu@users.noreply.github.com>
2024-05-30 15:23:42 +0200

    install gsd and hwl templates are now copied from .apax forlder

--- src/scripts/copy_hwl_templates.sh
+++ src/scripts/copy_hwl_templates.sh
@@ -1,20 +1,27 @@
-if [ -d "../ctrl/assets" ]; then
-  echo "Directory "../ctrl/assets" exists!!!"
+if [ -d "./.apax" ]; then
+  echo "Directory ".apax" exists!!!"
   if ! [[ -d "./hwc" ]]; then
     echo "Directory "./hwc" does not exist!!!"
     mkdir -p "./hwc"
   fi
-  sourceDirectory="../ctrl/assets"
-  destinationDirectory="./hwc"
-  fileMask="*.hwl.json"
-  files=($sourceDirectory/$fileMask)
-  if [ ${#files[@]} -gt 0 ] && [ "${files[0]}" != "$sourceDirectory/$fileMask" ]; then
-    echo " ${#files[@]}  files is going to be copied to $destinationDirectory."
-    cp $sourceDirectory/$fileMask $destinationDirectory
-    echo " ${#files[@]} files copied to $destinationDirectory."
-  else
-    echo "No files matching the pattern '$fileMask' were found in '$sourceDirectory'."
-  fi
+  
+    ASSETS_DIRS=$(find -L "./.apax" -type d -name 'assets')
+
+    for DIR in $ASSETS_DIRS; do      
+      sourceDirectory="$DIR"
+      destinationDirectory="./hwc"
+      fileMask="*.hwl.json"      
+      files=($sourceDirectory/$fileMask)
+      if [ ${#files[@]} -gt 0 ] && [ "${files[0]}" != "$sourceDirectory/$fileMask" ]; then
+        echo " ${#files[@]}  files is going to be copied to $destinationDirectory."
+        cp $sourceDirectory/$fileMask $destinationDirectory
+        echo " ${#files[@]} files copied to $destinationDirectory."
+      else
+        echo "No files matching the pattern '$fileMask' were found in '$sourceDirectory'."
+      fi
+  done
+
+  
 else
   echo "Directory "../ctrl/assets" does not exist!!!"
 fi
