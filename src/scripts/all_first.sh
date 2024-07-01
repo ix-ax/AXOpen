984c5942e78f815046d7263b63887498c282f04e
TK <61820360+TomKovac@users.noreply.github.com>
2024-07-01 17:11:17 +0200

    scripts improvements and cleanup

--- src/scripts/all_first.sh
+++ src/scripts/all_first.sh
@@ -39,6 +39,21 @@ export RED='\033[0;31m'
 
 printf "${RED}This command will prompt during execution, so do not leave your PC. You can enjoy your coffee afterward.\r\n"
 
+use_plcsim=AXUSEPLCSIM
+use_plcsim_value=$(printenv "$use_plcsim")
+
+if [ -z "$use_plcsim_value" ]; then
+	echo "Environment variable '$use_plcsim' is not set."
+else
+	echo "The value of '$use_plcsim' is: $use_plcsim_value"
+
+	if [ "$(echo 'true' | tr '[:upper:]' '[:lower:]')" == "$(echo "$use_plcsim_value" | tr '[:upper:]' '[:lower:]')" ]; then
+		plcsimscript=$( dirname ${BASH_SOURCE[0]})"\\StartPlcSimAdvCli.exe"
+		$plcsimscript
+	fi
+fi
+
+
 #apax run ci                                  # clean and install dependencies
 apax clean
 apax install
