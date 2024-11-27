fa65d060acfa49e9f9ab51f13580929f81849f96
PTKu <61538034+PTKu@users.noreply.github.com>
2024-11-27 10:02:36 +0100

    Refactor VisualComposer to use direct ID comparison and remove unnecessary hashing

--- src/base/src/AXOpen.VisualComposer/VisualComposerContainer.razor.cs
+++ src/base/src/AXOpen.VisualComposer/VisualComposerContainer.razor.cs
@@ -258,7 +258,7 @@ namespace AXOpen.VisualComposer
 
                 foreach (var item in deserialize.Items)
                 {
-                    var childObject = _childrenOfAxoObject.FirstOrDefault(p => p.Symbol.ModalIdHelper().ComputeSha256Hash() == item.Id);
+                    var childObject = _childrenOfAxoObject.FirstOrDefault(p => p.Symbol.ModalIdHelper() == item.Id);
                     if (childObject != null)
                     {
                         _children.Add(new VisualComposerItemData(EventCallback.Factory.Create(this, StateHasChanged), EventCallback.Factory.Create(this, SaveAsync), childObject, childObject.Symbol.ModalIdHelper(), Guid.NewGuid(), item.Left, item.Top, Types.TransformType.FromString(item.Transform), item.Presentation, item.Width, item.Height, item.ZIndex, item.Scale, item.Roles, item.PresentationTemplate, item.Background, item.BackgroundColor));
