c46e6eca98ad3495398049ea07e5259f17aeca54
TK <61820360+TomKovac@users.noreply.github.com>
2024-12-09 18:30:50 +0100

    dll removed, used the local one dynamically

--- src/tools/src/PlcSimAdvancedStarter/PlcSimAdvancedStarterTool/PlcSim/PlcSimInstance.cs
+++ src/tools/src/PlcSimAdvancedStarter/PlcSimAdvancedStarterTool/PlcSim/PlcSimInstance.cs
@@ -10,19 +10,18 @@ namespace PlcSimAdvancedStarterTool.PlcSim
     public class PlcSim
     {
         [Command("startplcsim")]
-        public Task StartPlcSim(  [Option('x',Description = "PlcSim instance name")] string PlcSimInstanceName,
-                                        [Option('n',Description = "Plc name.")] string PlcName,
-                                        [Option('t',Description = "Plc Ip address")] string PlcIpAddress)
+        public Task StartPlcSim([Option('x', Description = "PlcSim instance name")] string PlcSimInstanceName,
+                                        [Option('n', Description = "Plc name.")] string PlcName,
+                                        [Option('t', Description = "Plc Ip address")] string PlcIpAddress)
         {
             string dllPath = "";
             string exePath = "";
 
-            if(PlcSimInstallation.Check(ref dllPath, ref exePath))
+            if (PlcSimInstallation.Check(ref dllPath, ref exePath))
             {
                 // This is a workaround to get the local dll for the PlcSimAdvanced API
                 var entry = new FileInfo(Assembly.GetEntryAssembly().Location);
                 var folder = entry.Directory.FullName;
-                File.Copy(dllPath, Path.Combine(folder, "Siemens.Simatic.Simulation.Runtime.Api.x64.dll"), true);
 
                 // Check if PlcSim is already running
                 if (Process.GetProcesses().Where(p => p.ProcessName.Equals("Siemens.Simatic.PlcSim.Advanced.UserInterface")).Count() > 0)
@@ -36,10 +35,117 @@ namespace PlcSimAdvancedStarterTool.PlcSim
                     Console.WriteLine("PlcSimAdvanced started.");
                 }
 
-                exePath = Path.Combine(folder, "PlcSimAdvancedStarter.exe");
-                Process.Start(new ProcessStartInfo(exePath) { Arguments = $"-x {PlcSimInstanceName} -n {PlcName} -t {PlcIpAddress}" });
-            }
+                var plcSimAdvancedApiDll = Assembly.LoadFrom(dllPath);
 
+                var simulationRuntimeManager = plcSimAdvancedApiDll.GetType("Siemens.Simatic.Simulation.Runtime.SimulationRuntimeManager");
+                var networkModeProperty = simulationRuntimeManager.GetRuntimeProperty("NetworkMode");
+                var eNetworkModeType = plcSimAdvancedApiDll.GetType("Siemens.Simatic.Simulation.Runtime.ENetworkMode");
+                var tcpipSingleAdapterValue = Enum.Parse(eNetworkModeType, "TCPIPSingleAdapter");
+                networkModeProperty.SetValue(null, tcpipSingleAdapterValue);
+
+                // Check if PlcSimInstanceName is already registered
+                bool instanceAlreadyRegistered = false;
+
+
+                var iInstanceType = plcSimAdvancedApiDll.GetType("Siemens.Simatic.Simulation.Runtime.IInstance");
+                object plcSimInstance = null;
+
+                var registeredInstanceInfoProperty = simulationRuntimeManager.GetProperty("RegisteredInstanceInfo", BindingFlags.Static | BindingFlags.Public);
+                var instanceInfos = registeredInstanceInfoProperty.GetValue(null) as Array;
+                if (instanceInfos != null)
+                {
+                    foreach (var instanceInfo in instanceInfos)
+                    {
+                        string instanceName = instanceInfo.GetType().GetField("Name", BindingFlags.Public | BindingFlags.Instance).GetValue(instanceInfo)?.ToString();
+
+                        if (instanceName.Equals(PlcSimInstanceName))
+                        {
+                            Console.WriteLine($"Instance {PlcSimInstanceName} already registered.");
+                            var createInterfaceMethod = simulationRuntimeManager.GetMethod("CreateInterface", BindingFlags.Static | BindingFlags.Public, null, new Type[] { typeof(string) }, null);
+                            plcSimInstance = createInterfaceMethod.Invoke(null, new object[] { PlcSimInstanceName });
+                            instanceAlreadyRegistered = true;
+                            break;
+                        }
+                    }
+                }
+
+
+                // Register PlcSimInstanceName
+                if (!instanceAlreadyRegistered)
+                {
+                    var registerInstanceMethod = simulationRuntimeManager.GetMethod("RegisterInstance", BindingFlags.Static | BindingFlags.Public, null, new Type[] { typeof(string) }, null);
+                    plcSimInstance = registerInstanceMethod.Invoke(null, new object[] { PlcSimInstanceName });
+                    Console.WriteLine($"Instance {PlcSimInstanceName} registered.");
+                }
+
+                // Power On 
+                var eOperatingStateType = plcSimAdvancedApiDll.GetType("Siemens.Simatic.Simulation.Runtime.EOperatingState");
+                string operatingStateTypeOffValue = Enum.Parse(eOperatingStateType, "Off").ToString();
+                string operatingStateValue = plcSimInstance.GetType().GetRuntimeProperty("OperatingState").GetValue(plcSimInstance).ToString();
+
+                if (operatingStateValue.Equals(operatingStateTypeOffValue))
+                {
+                    var powerOnMethod = plcSimInstance.GetType().GetMethod("PowerOn", BindingFlags.Public | BindingFlags.Instance, null, new Type[] { typeof(uint) }, null);
+                    uint timeout = 6000;
+                    powerOnMethod.Invoke(plcSimInstance, new object[] { timeout });
+                    Console.WriteLine($"Instance {PlcSimInstanceName} powered on.");
+                }
+                else
+                {
+                    Console.WriteLine($"Instance {PlcSimInstanceName} already powered.");
+                }
+
+                //  Set Ip address
+                var sipSuiteType = plcSimInstance.GetType().Assembly.GetType("Siemens.Simatic.Simulation.Runtime.SIPSuite4");
+
+                if (sipSuiteType != null)
+                {
+                    var setIpSuiteMethod = plcSimInstance.GetType().GetMethod("SetIPSuite", BindingFlags.Public | BindingFlags.Instance);
+                    if (setIpSuiteMethod != null)
+                    {
+                        var sipSuiteInstance = Activator.CreateInstance(sipSuiteType, new object[] { PlcIpAddress, "255.255.255.0", "0.0.0.0" });
+                        if (sipSuiteInstance != null)
+                        {
+                            setIpSuiteMethod.Invoke(plcSimInstance, new object[] { (UInt32)0, sipSuiteInstance, true });
+                        }
+                        else
+                        {
+                            Console.WriteLine("Failed to create SIPSuite4 instance.");
+                            return Task.CompletedTask;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("SetIPSuite method not found.");
+                        return Task.CompletedTask;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("SIPSuite4 type not found.");
+                    return Task.CompletedTask;
+                }
+
+                // Run
+                string operatingStateTypeStopValue = Enum.Parse(eOperatingStateType, "Stop").ToString();
+                operatingStateValue = plcSimInstance.GetType().GetRuntimeProperty("OperatingState").GetValue(plcSimInstance).ToString();
+
+                if (operatingStateValue.Equals(operatingStateTypeStopValue))
+                {
+                    try
+                    {
+                        var runMethod = plcSimInstance.GetType().GetMethod("Run", BindingFlags.Public | BindingFlags.Instance, null, new Type[] { typeof(uint) }, null);
+                        uint timeout = 6000;
+                        runMethod.Invoke(plcSimInstance, new object[] { timeout });
+                        Console.WriteLine($"PLC set to RUN mode.");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Unable to set the PLC into the RUN mode. {ex.Message}");
+
+                    }
+                }
+            }
             return Task.CompletedTask;
         }
     }
