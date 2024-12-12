a630ac4d1771b19defeb246904cfe9a32c65e9c9
TK <61820360+TomKovac@users.noreply.github.com>
2024-12-12 17:08:48 +0100

    wip

---
+++ scripts/create_issue_branch.ps1
@@ -0,0 +1,37 @@
+param (
+    [int]$IssueId )
+
+gh issue list --assignee "@me" --state "open"
+$issues = gh issue list --state "open" --assignee "@me" --json number,title | ConvertFrom-Json
+$issueIDs = $issues | ForEach-Object { $_.number }
+
+if (-not $IssueId) {
+    $IssueId = Read-Host "Please enter an ID value of the issue"
+}
+
+
+if ([int]::TryParse($IssueId, [ref]$null)) {
+    if ($issueIDs -contains [int]$IssueId) {
+        $selectedIssue = $issues | Where-Object { $_.number -eq [int]$IssueId }
+        $selectedIssueNumber = $selectedIssue.number
+        $selectedIssueTitle = $selectedIssue.title
+        Write-Output "Creating branch for the issue number: '$selectedIssueNumber', title: '$selectedIssueTitle'"
+        gh issue develop $IssueId --base dev --checkout
+        git add .
+        git commit -m "Create draft PR for #$selectedIssueNumber"
+        Write-Output "Pushing the branch to remote"
+        git push -u origin $(git branch --show-current)
+        Write-Output "Creating a draft pull request into 'dev'"
+        gh pr create --base dev --head $(git branch --show-current) --title "$selectedIssueTitle" --body "closes #$selectedIssueNumber" --draft
+        git push 
+        Write-Output "Sync local and remote branches"
+        git pull origin $(git branch --show-current)
+        git push 
+    } else {
+        Write-Output "Error: The issue ID '$IssueId' does not exist in the list of open issues."
+        exit 1
+    }
+} else {
+    Write-Output "Error: The value '$IssueId' is not a valid numeric value."
+    exit 1
+}
