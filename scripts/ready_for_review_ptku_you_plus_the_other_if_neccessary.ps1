3a2ec56dc7f92d8ff220988e2e08eb9a7b263507
TK <61820360+TomKovac@users.noreply.github.com>
2024-12-13 00:25:30 +0100

    wip

--- scripts/ready_for_review_ptku_you_plus_the_other_if_neccessary.ps1
+++ scripts/ready_for_review_ptku_you_plus_the_other_if_neccessary.ps1
@@ -6,6 +6,11 @@ if($gitStatus -notcontains "nothing to commit, working tree clean")
 else
 {
     $currentBranch = $(git branch --show-current)
+    Write-Output "Working tree is clean. Marking '$currentBranch' as ready fore review"
+    $AdditionalReviewers = Read-Host "Please enter the additional reviewers names: i.e.: my_boss_github_name, his_boss_github_name, etc. (upto the galaxy owner)"
+    
+
     Write-Output "currentBranch:  $currentBranch"
+    Write-Output "AdditionalReviewers:  $AdditionalReviewers"
 }
 
