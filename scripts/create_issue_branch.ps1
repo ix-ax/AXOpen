03a1f9ea5ae48df811c7834000aeba6b635822cd
TK <61820360+TomKovac@users.noreply.github.com>
2024-12-12 14:30:53 +0100

    cib

---
+++ scripts/create_issue_branch.ps1
@@ -0,0 +1,22 @@
+gh issue lists --assignee "@me" --state "open"
+$issueIDs = gh issue list --state "open" --assignee "@me" --json number | ConvertFrom-Json | ForEach-Object { $_.number }
+$value = Read-Host "Please enter an ID value of the issue."
+
+if ([int]::TryParse($value, [ref]$null)) {
+    if ($issueIDs -contains [int]$value) {
+        Write-Output "Creating branch for the issue with ID: '$value'"
+        gh issue develop $value --base dev --checkout
+        Write-Output "Pushing the branch to remote"
+        git push -u origin $(git branch --show-current)
+        Write-Output "Creating a draft pull request into 'dev'"
+        gh pr create --base dev --head $(git branch --show-current) --title "closes $value" --body "Create draft PR for #$value" --draft
+        Write-Output "Sync local and remote branches"
+        git pull origin $(git branch --show-current)
+    } else {
+        Write-Output "Error: The issue ID '$value' does not exist in the list of open issues."
+        exit 1
+    }
+} else {
+    Write-Output "Error: The value '$value' is not a valid numeric value."
+    exit 1
+}
