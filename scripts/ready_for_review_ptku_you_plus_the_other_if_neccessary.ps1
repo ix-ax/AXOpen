6c584ec85697dec82394d70a27c968f5637fc275
TK <61820360+TomKovac@users.noreply.github.com>
2024-12-13 00:46:25 +0100

    wip

--- scripts/ready_for_review_ptku_you_plus_the_other_if_neccessary.ps1
+++ scripts/ready_for_review_ptku_you_plus_the_other_if_neccessary.ps1
@@ -8,6 +8,7 @@ else
     $currentBranch = $(git branch --show-current)
     Write-Output "Working tree is clean. Marking '$currentBranch' as ready fore review"
     $AdditionalReviewers = Read-Host "Please enter the additional reviewers names: i.e.: my_boss_github_name, his_boss_github_name, etc. (upto the galaxy owner)"
+    $ReviewComment = Read-Host "Please enter a comment for the reviewers, for empty press enter"
     Write-Output "currentBranch:  $currentBranch"
     Write-Output "AdditionalReviewers:  $AdditionalReviewers"
     $Reviewers = "PTKu,@me" + "," + $AdditionalReviewers
@@ -15,5 +16,9 @@ else
     $ReviewersString = $ReviewersArray -join ","
     $Command = "gh pr review-request --add " + $ReviewersString
     Invoke-Expression $Command
+    if ($ReviewComment -ne "") {
+        $Command = "gh pr comment -b """ + $ReviewComment + """"
+        Invoke-Expression $Command
+    }
 }
 
