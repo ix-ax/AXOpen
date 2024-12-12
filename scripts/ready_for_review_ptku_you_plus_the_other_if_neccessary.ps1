d233772a52a6d2f048e56e91b42979acf6bff394
TK <61820360+TomKovac@users.noreply.github.com>
2024-12-13 00:19:30 +0100

    wip

--- scripts/ready_for_review_ptku_you_plus_the_other_if_neccessary.ps1
+++ scripts/ready_for_review_ptku_you_plus_the_other_if_neccessary.ps1
@@ -2,5 +2,10 @@ $gitStatus = git status
 if($gitStatus -notcontains "nothing to commit, working tree clean")
 {
     Write-Output "Working tree is not clean. Commit and push your local changes and run this script again afterwards."
-
 }
+else
+{
+    $currentBranch = $(git branch --show-current)
+    Write-Output "currentBranch:  $currentBranch"
+}
+
