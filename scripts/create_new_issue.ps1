bce8b67ada5ae930c5d80abe785f4538f0d69333
TK <61820360+TomKovac@users.noreply.github.com>
2024-12-12 18:27:56 +0100

    relative path in script

--- scripts/create_new_issue.ps1
+++ scripts/create_new_issue.ps1
@@ -11,7 +11,14 @@ $issue = gh issue create --assignee "@me" --title "$IssueTitle" --body "$IssueBo
 if ($issue -match ".*/(\d+)$") {
     $issueID = $matches[1] 
     Write-Output "The issue ID is: $issueID"
-    .\create_issue_branch.ps1 -IssueId $issueID
+    # Get the current script directory
+    $scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+
+    # Construct the full path to SecondScript.ps1
+    $create_issue_branchScriptPath = Join-Path -Path $scriptDir -ChildPath "create_issue_branch.ps1"
+
+    # Call create_issue_branch.ps1 with the parameter IssueId
+    & $create_issue_branchScriptPath -IssueId $issueID
 } else {
     Write-Output "No numeric ID found for this issue."
 }
