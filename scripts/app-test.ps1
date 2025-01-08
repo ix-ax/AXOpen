779b2bc888a395d1fc0db2a3e08b7a866f5a2f80
TK <61820360+TomKovac@users.noreply.github.com>
2025-01-08 15:37:40 +0100

    error message at the end of ps script added

--- scripts/app-test.ps1
+++ scripts/app-test.ps1
@@ -801,6 +801,7 @@ if ($appYamls)
         Write-Error "Failed to create the log file."
     }
     Kill-Process -ProcessName "Siemens.Simatic.PlcSim.Advanced.UserInterface"
+    Write-Error "I am just curious how this error message impact github action status."
 } 
 else 
 {
