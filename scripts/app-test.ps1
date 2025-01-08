c76eeecce65f79499f667082aa05fdc53f0f4446
TK <61820360+TomKovac@users.noreply.github.com>
2025-01-08 19:25:13 +0100

    wip

--- scripts/app-test.ps1
+++ scripts/app-test.ps1
@@ -335,13 +335,14 @@ function Run-Command {
     }
 
     try {
-        # Execute the command and capture the output
-        $CommandOutput = Invoke-Expression "$Command 2>&1" | ForEach-Object {
-            # Write each line to the console immediately
-            #Write-Output $_
-            # Add it to the result output
-            $_
-        }
+        Invoke-Expression $Command 2>&1
+        ## Execute the command and capture the output
+        #$CommandOutput = Invoke-Expression "$Command 2>&1" | ForEach-Object {
+        #    # Write each line to the console immediately
+        #    Write-Output $_
+        #    # Add it to the result output
+        #    $_
+        #}
 
         $result.Output = $CommandOutput
 
