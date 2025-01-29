5072806e8b6845ea08015f466119e4e8795cb8ab
blazej.kuhajda <blazej.kuhajda@mts.sk>
2025-01-29 07:57:45 +0100

    added missing localization

--- src/security/integrations/axopen_blazor_auth_app/Program.cs
+++ src/security/integrations/axopen_blazor_auth_app/Program.cs
@@ -17,7 +17,7 @@ using AXOpen.Data.MongoDb;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+builder.Services.AddLocalization();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
