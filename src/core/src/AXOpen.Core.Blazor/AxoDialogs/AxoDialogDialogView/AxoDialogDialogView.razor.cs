8b2be1b0e213b153e86a9b7868f0b95833c37693
PTKu <61538034+PTKu@users.noreply.github.com>
2025-06-13 16:11:00 +0200

    Enhance dialog background color handling and improve UI layout in inspector dialog

--- src/core/src/AXOpen.Core.Blazor/AxoDialogs/AxoDialogDialogView/AxoDialogDialogView.razor.cs
+++ src/core/src/AXOpen.Core.Blazor/AxoDialogs/AxoDialogDialogView/AxoDialogDialogView.razor.cs
@@ -1,4 +1,5 @@
-﻿using AXOpen.Core.Blazor.AxoDialogs;
+﻿using System.Security.Principal;
+using AXOpen.Core.Blazor.AxoDialogs;
 using AXSharp.Connector;
 
 namespace AXOpen.Core
@@ -22,6 +23,29 @@ namespace AXOpen.Core
         }
 
 
+
+        private string BackgroundColor
+        {
+            get
+            {
+                var parsedEnum = (eDialogType)Enum.Parse(typeof(eDialogType), Component._dialogType.Cyclic.ToString());
+                switch (parsedEnum)
+                {
+                    case eDialogType.Info:
+                        return "bg-info-500"; // light red
+                    case eDialogType.Warning:
+                        return "bg-warning-500"; // light yellow
+                    case eDialogType.Danger:
+                        return "bg-danger-500"; // light blue
+                    case eDialogType.Undefined:
+                        return "bg-info-500"; // light green
+                    default:
+                        return "bg-info-500"; // default white
+                }
+            }
+        }
+
+
         public override void ConfigurePolling()
         {
            this.StartPolling(this.Component, 250); // call always "base"
@@ -30,14 +54,14 @@ namespace AXOpen.Core
 
             if (dialog != null)
             {
-                var selecedToPool = new List<ITwinElement>();
+                var toPool = new List<ITwinElement>();
 
-                selecedToPool.Add(dialog._dialogType);
-                selecedToPool.Add(dialog._buttons);
-                selecedToPool.Add(dialog._caption);
-                selecedToPool.Add(dialog._text); // can be changed
+                toPool.Add(dialog._dialogType);
+                toPool.Add(dialog._buttons);
+                toPool.Add(dialog._caption);
+                toPool.Add(dialog._text); // can be changed
 
-                foreach (var item in selecedToPool)
+                foreach (var item in toPool)
                 {
                     StartPolling(item);
                     PolledElements.Add(item);
@@ -46,25 +70,35 @@ namespace AXOpen.Core
 
         }
 
-
         public async Task DialogAnswerOk()
         {
             Component._answer.Edit = (short)eDialogAnswer.OK;
+            AxoApplication.Current.Logger.Information($"User answered `OK`, to question {Component._text.LastValue} [{Component._caption.LastValue}]", Component, await GetUserIdentity());
             await CloseDialogsWithSignalR();
         }
+
+        private async Task<IIdentity?> GetUserIdentity()
+        {
+            var authenticationStatus = await _asp.GetAuthenticationStateAsync();
+            return authenticationStatus.User.Identity;
+        }
+
         public async Task DialogAnswerYes()
         {
             Component._answer.Edit = (short)eDialogAnswer.Yes;
+            AxoApplication.Current.Logger.Information($"User answered `YES`, to question {Component._text.LastValue} [{Component._caption.LastValue}]", Component, await GetUserIdentity());
             await CloseDialogsWithSignalR();
         }
         public async Task DialogAnswerNo()
         {
             Component._answer.Edit = (short)eDialogAnswer.No;
+            AxoApplication.Current.Logger.Information($"User answered `NO`, to question {Component._text.LastValue} [{Component._caption.LastValue}]", Component, await GetUserIdentity());
             await CloseDialogsWithSignalR();
         }
         public async Task DialogAnswerCancel()
         {
             Component._answer.Edit = (short)eDialogAnswer.Cancel;
+            AxoApplication.Current.Logger.Information($"User answered `CANCEL`, to question {Component._text.LastValue} [{Component._caption.LastValue}]", Component, await GetUserIdentity());
             await CloseDialogsWithSignalR();
         }
 
