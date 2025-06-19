d8b474ea067d9ebbc4da097be4eb710b3fed15ef
PTKu <61538034+PTKu@users.noreply.github.com>
2025-06-19 17:23:49 +0200

    fixing broken stuff after operon refactoring

--- src/template.axolibrary/app/ix-blazor/Program.cs
+++ src/template.axolibrary/app/ix-blazor/Program.cs
@@ -27,8 +27,8 @@ builder.Services.AddAxoCoreServices();
 
 Entry.Plc.Connector.SubscriptionMode = ReadSubscriptionMode.Polling;
 Entry.Plc.Connector.BuildAndStart().ReadWriteCycleDelay = 250;
-Entry.Plc.Connector.ConcurrentRequestMaxCount = 4; 
-Entry.Plc.Connector.ConcurrentRequestDelay = 100;
+//Entry.Plc.Connector.ConcurrentRequestMaxCount = 4; 
+//Entry.Plc.Connector.ConcurrentRequestDelay = 100;
 Entry.Plc.Connector.ExceptionBehaviour = CommExceptionBehaviour.ReThrow;
 
 Entry.Plc.Connector.SetLoggerConfiguration(new LoggerConfiguration()
