97b7a487f82427e97f26dd2b7a650321f237a2a8
PTKu <61538034+PTKu@users.noreply.github.com>
2025-09-23 09:26:57 +0200

    Refactor namespaces to AXOpen.Components.Elements
    Updated multiple files to change namespaces from
    AXOpen.Elements to AXOpen.Components.Elements for
    better organization and modularity. Adjusted settings
    in AXSharp.config.json and rearranged folder paths
    in components.elements.code-workspace.

--- src/components.elements/src/AXOpen.Components.Elements/AxoCarousel/AxoCarousel.cs
+++ src/components.elements/src/AXOpen.Components.Elements/AxoCarousel/AxoCarousel.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AXOpen.Elements
+namespace AXOpen.Components.Elements
 {
     public partial class AxoCarousel
     {
