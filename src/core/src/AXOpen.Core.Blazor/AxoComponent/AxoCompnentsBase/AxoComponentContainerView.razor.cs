5c22c69442ff596424b26e902e4fa53c8a879847
Peter Kurhajec <61538034+PTKu@users.noreply.github.com>
2025-11-21 09:40:57 +0100

    Spot view lable restyled Enhance component styling: update label background class and adjust text styling for better visibility

--- src/core/src/AXOpen.Core.Blazor/AxoComponent/AxoCompnentsBase/AxoComponentContainerView.razor.cs
+++ src/core/src/AXOpen.Core.Blazor/AxoComponent/AxoCompnentsBase/AxoComponentContainerView.razor.cs
@@ -140,6 +140,17 @@ namespace AXOpen.Core.Blazor
                _ => ""
            };
 
+        public string LabelBackgroundClass =>
+           _alarmLevel switch
+           {
+               eAlarmLevel.NoAlarms => "bg-background/80",
+               eAlarmLevel.Unacknowledged => "bg-warning",
+               eAlarmLevel.ActiveInfo => "bg-info",
+               eAlarmLevel.ActiveWarnings => "bg-warning/20! shadow-glow-warning",
+               eAlarmLevel.ActiveErrors => "bg-danger/20! shadow-glow-danger",
+               _ => "bg-background/80"
+           };
+
         public bool AlarmsActive { get; private set; }
 
         public void SetView(eViewType? viewType)
