98c4eb0f4d3cbe5e2c4197604340d0ab9a5356ea
TK <61820360+TomKovac@users.noreply.github.com>
2025-12-03 21:27:11 +0100

    wip

--- scripts/_is_on_dev_nothing_to_commit.ps1
+++ scripts/_is_on_dev_nothing_to_commit.ps1
@@ -3,24 +3,24 @@ if($currentBranch -ne "dev")
 {
     Write-Output "You are not currently on the 'dev' branch."
     Write-Host "You are not currently on the 'dev' branch, but $currentBranch " -ForegroundColor Red
-    return $false
+    return -1
 }
 else
 {
     Write-Host "You are currently on the 'dev' branch" -ForegroundColor Green
 }
-$isClean =$false
+$isClean = -1
 $status = git status
 foreach ($statusLine in $status ) 
 {
     if ($statusLine -eq "nothing to commit, working tree clean") 
     {
-        $isClean = $true
+        $isClean = 1
         Write-Host "Nothing to commit, working tree clean" -ForegroundColor Green
         break
     }
 }
-if(-not $isClean)
+if($isClean -ne 1)
 {
     Write-Host "You have some uncommited changes on the 'dev' branch" -ForegroundColor Red
 }
