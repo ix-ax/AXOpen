f898d0230b7d72ca9ee8b3bf7130d74bfbdf8de9
Peter Kurhajec <61538034+PTKu@users.noreply.github.com>
2025-12-11 11:49:02 +0100

    Add Controller API documentation and update API index

--- scripts/build_test_docu.ps1
+++ scripts/build_test_docu.ps1
@@ -17,5 +17,9 @@ if ((Test-Path .\docs-test\)) {
     del .\docs-test\
 }
 
+# Generate metadata (API documentation) from C# projects
+# dotnet docfx metadata .\docfx\docfx.json
+
+# Build the documentation site
 dotnet docfx build .\docfx\docfx.json --output .\docs-test\
 dotnet docfx serve .\docs-test\ --open-browser
