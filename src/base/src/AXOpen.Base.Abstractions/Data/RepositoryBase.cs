9c96716778d432767e212264991f14e9daa687c8
Peter Kurhajec <61538034+PTKu@users.noreply.github.com>
2026-01-14 13:05:01 +0100

    Safely set ModifiedAt using null-conditional operator
    Prevent possible NullReferenceException by using the null-conditional operator when assigning to data.ModifiedAt. This ensures the assignment only occurs if the data object is not null.

--- src/base/src/AXOpen.Base.Abstractions/Data/RepositoryBase.cs
+++ src/base/src/AXOpen.Base.Abstractions/Data/RepositoryBase.cs
@@ -351,8 +351,8 @@ namespace AXOpen.Base.Data
                 }
                 try
                 {
-                    data.CreatedAt = DateTime.UtcNow;
-                    data.ModifiedAt = DateTime.UtcNow;
+                    data?.CreatedAt = DateTime.UtcNow;
+                    data?.ModifiedAt = DateTime.UtcNow;
                     CreateNvi(identifier, data);
                 }
                 catch (Exception e)
@@ -411,7 +411,7 @@ namespace AXOpen.Base.Data
                 }
                 try
                 {
-                    data.ModifiedAt = DateTime.UtcNow;
+                    data?.ModifiedAt = DateTime.UtcNow;
                     UpdateNvi(identifier, data);
                 }
                 catch (Exception e)
