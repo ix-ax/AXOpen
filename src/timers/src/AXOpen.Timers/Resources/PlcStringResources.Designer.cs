20bee73da7756db81cd1ae70d2bdec13997c8e5e
Peter Kurhajec <61538034+PTKu@users.noreply.github.com>
2026-01-28 16:49:07 +0100

    Add ManifestResourceName for PlcStringResources in multiple projects
    - Updated the .csproj files for various components (Balluff, Cognex, Desoutter, Drives, Elements, Festo, Keyence, Kuka, Mitsubishi, Pneumatics, Rexroth, Robotics, Siem, Ur) to include the ManifestResourceName for the embedded resource PlcStringResources.resx.
    - Added new resource files for PlcStringResources in AXOpen.Abstractions, AXOpen.Data, AXOpen.Timers, AXOpen.Utils, and AXOpen.S71500.
    - Enhanced AxOpenCoreResources.Designer.cs with additional localized strings for better internationalization support.
    - Updated _Imports.razor to include AXOpen.Core.Resources for localization.

--- src/timers/src/AXOpen.Timers/Resources/PlcStringResources.Designer.cs
+++ src/timers/src/AXOpen.Timers/Resources/PlcStringResources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace AXOpen.Components.Abstractions.Resources {
+namespace AXOpen.Timers.Resources {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace AXOpen.Components.Abstractions.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AXOpen.Components.Abstractions.Resources.PlcStringResources", typeof(PlcStringResources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AXOpen.Timers.Resources.PlcStringResources", typeof(PlcStringResources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
