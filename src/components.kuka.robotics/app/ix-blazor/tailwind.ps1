46118d74c55b8cce748b5c262f02ba0c12fe237e
TK <61820360+TomKovac@users.noreply.github.com>
2026-02-09 13:02:26 +0100

    wip

--- src/components.kuka.robotics/app/ix-blazor/tailwind.ps1
+++ src/components.kuka.robotics/app/ix-blazor/tailwind.ps1
@@ -1,1 +1,1 @@
-npx @tailwindcss/cli -i .\wwwroot\css\App.css -o .\wwwroot\css\tailwind\tailwind.css --watch --content ".\**\*.{html,js,jsx,ts,tsx,vue,razor}"
+npx @tailwindcss/cli -i .\wwwroot\css\tailwind.css -o .\wwwroot\css\momentum.css --watch
