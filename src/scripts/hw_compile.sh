ee4422395c114558a572e68c14e166ad82fd0738
TK <61820360+TomKovac@users.noreply.github.com>
2026-02-20 08:18:21 +0100

    normalize line endings for hwc.gen files in copy_hardware_ids.sh, copy_io_addresses.sh, and hw_compile.sh

--- src/scripts/hw_compile.sh
+++ src/scripts/hw_compile.sh
@@ -5,7 +5,7 @@ export NC='\033[0m\r\n' # No Color+CRLF
 hwcc=$(apax hwc compile -i ".\hwc" -o bin/hwc/)
 dos2unix SystemConstants/*
 dos2unix -r src/IO/*
-dos2unix -r hwc/hwc.gen/
+dos2unix -r hwc/hwc.gen/*
 if [[ $? -eq 0 ]]; then
 	printf "${GREEN}Hardware configuration compiled succesfully.${NC}"
 	exit 0
